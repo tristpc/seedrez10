cec549819df5a7afcaebf207fd1e3973fdd7e0fa
Paul Cardwell <paulcardwell@gmail.com>
2020-02-07 14:53:15 -0600

    TourList

--- src/templates/TourList.js
+++ src/templates/TourList.js
@@ -1,129 +1,119 @@
-// https://www.gatsbyjs.org/docs/adding-tags-and-categories-to-blog-posts/#add-tags-to-your-markdown-files
-//! DIFFERENCE WITH NORMAL TOURLIST IS QRAPHQL WITH SIMPLER FILTER
-//! Copy of ToursIndexPage4.js, but changing CSS from MUI
-//! Adds location from @reach/router as TourIndex.js
-
-//! This creates region/tag pages, eg http://localhost:8000/osa/adventure/
-
-
-//* full version for tags
-import React from 'react'; //! remove useState
-import { graphql } from 'gatsby';
+//version of BlogIndex.js'
+import React from 'react'
+import { graphql } from 'gatsby'
 import { Location } from '@reach/router'
 import qs from 'qs'
 
-
-//import TourRegionNav from '../components/TourRegionNav' //version of PostCategoriesNav
-import ListPageHeader from '../components/ListPageHeader' //version of PageHeader
+import SEO from "../components/seo"
+import TourSection from '../components/PostSection'
+//import PostCategoriesNav from '../components/PostCategoriesNav'
 import Layout from '../components/Layout'
-// import TourTagsNav from '../components/TourTagsNav' //version of PostCategoriesNav
-// import TourRegionsNav from '../components/TourRegionsNav' //version of TourTagsNav
-import TourSection from '../components/TourSection' //simple function version of PostSection
-
-//! changes re sidebar
 import SideBar from '../components/SideBarTest'
-// import DropMenuButton from "../components/DropMenuButton";
-// import DropMenu from "../components/DropMenu";
 
-
+// Export Template for use in CMS preview
 const PlaceCatIndex = ({ pageContext, data }) => (
-
   <Location>
     {({ location }) => {
+      const { tag, place, tagsRegion, allRegions } = pageContext;
 
-    const { tag, place, tagsRegion, allRegions } = pageContext;
+      let { edges: posts, totalCount } = data.allMdx;
+      const arrTagsRegion = tagsRegion.split(",")
+      const arrAllRegions = allRegions.split(",")
+  
+      let enableSearch = true,
+      filteredPosts = posts
+  
+      //* Add filter
+      let queryObj = location.search.replace('?', '')
+        queryObj = qs.parse(queryObj)
+  
+      if (enableSearch && queryObj.s) {
+        const searchTerm = queryObj.s.toLowerCase()
+        filteredPosts = posts.filter(post =>
+          post.node.frontmatter.title.toLowerCase().includes(searchTerm)
+        )
+      }
 
-    let { edges: posts, totalCount } = data.allMdx;
-    const arrTagsRegion = tagsRegion.split(",")
-    const arrAllRegions = allRegions.split(",")
+      return (
+        <main className="Places">
 
-    let enableSearch = true,
-    filteredPosts = posts
-
-    //* Add filter
-    let queryObj = location.search.replace('?', '')
-      queryObj = qs.parse(queryObj)
-
-    if (enableSearch && queryObj.s) {
-      const searchTerm = queryObj.s.toLowerCase()
-      filteredPosts = posts.filter(post =>
-        post.node.frontmatter.title.toLowerCase().includes(searchTerm)
-      )
-    }
-
-  return (
-    <Layout>
-
-      <main className="Blog">
-            <ListPageHeader
-              place={place}
-              tag={tag}
-              count = {totalCount}
-              //backgroundImage={featuredImage}
-            />
-
-
-            {/*TOUR LISTING & SIDEBAR USING COMPONENTS WITH HOOKS */}
-            {!!filteredPosts.length && (
-              <section className="section">
-                <div className="container">
-                  <div className="mainblock">
-                    <div className="aside">
-                      <SideBar enableSearch
-                      regions={arrAllRegions}
-                      tags={arrTagsRegion}
-                      place = {place}
-                      tag={tag} />
-                    </div>
-                    <div className="content">
-                      <TourSection posts={filteredPosts} />
-                    </div>
+          
+          {!!posts.length && (
+            <section className="section">
+              <div className="container">
+                <div className="mainblock">                
+                  <div className="aside">
+                    <SideBar />
+                  </div>
+                  <div className="content">
+                    <TourSection posts={filteredPosts} />
                   </div>
                 </div>
-              </section>
-            )}
-
-      </main>
-
-    </Layout>
-  );
-  }}
+              </div>
+            </section>
+          )}
+        </main>
+      )
+    }}
   </Location>
 )
-
 export default PlaceCatIndex;
 
+// Export Default ToursIndx for front-end
+const ToursIndx = ({ data: { posts } }) => (
+
+  <Layout>
+    <SEO
+        title="Tours list"
+        keywords={[`tours`, `regions`]}
+        description="The best tours in the region"
+        lang="English"
+    />
+
+    <PlaceCatIndex
+      posts={posts.edges.map(post => ({
+        ...post.node,
+        ...post.node.frontmatter,
+        ...post.node.fields
+      }))}
+    />
+  </Layout>
+)
+
+export default ToursIndx
+
 export const pageQuery = graphql`
-query($tag: String, $place: String) {
-  allMdx(
-    limit: 2000
-    sort: { fields: [frontmatter___price_from], order: ASC }
-    filter: {frontmatter: {tags: { in: [$tag] } meeting: { eq: $place } }}
-  ) {
-        totalCount
-        edges {
-          node {
-            excerpt
-            fields {
-              slug
-            }
-            frontmatter {
-              tourId
-              title
-              date
-              tags
-              price_from
-              featuredImage {
-                childImageSharp {
-                    fluid(maxWidth: 786) {
-                    ...GatsbyImageSharpFluid
-                    }
-                }
+  ## query name must be unique to this file
+  query($tag: String, $place: String) {
+    allMdx(
+      limit: 2000
+      sort: { fields: [frontmatter___price_from], order: ASC }
+      filter: {frontmatter: {tags: { in: [$tag] } meeting: { eq: $place } }}
+    ) {
+          totalCount
+          edges {
+            node {
+              excerpt
+              fields {
+                slug
               }
-              meeting
+              frontmatter {
+                tourId
+                title
+                date
+                tags
+                price_from
+                featuredImage {
+                  childImageSharp {
+                      fluid(maxWidth: 786) {
+                      ...GatsbyImageSharpFluid
+                      }
+                  }
+                }
+                meeting
+              }
             }
           }
         }
-      }
-  }
-`;
+    }
+`
